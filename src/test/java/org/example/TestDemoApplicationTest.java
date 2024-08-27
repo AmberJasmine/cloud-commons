cfa6833daf7a82b490e5c4dd753dc2f6ca24aff3
chenlu <1481094533@qq.com>
2024-08-27 19:30:44 +0800

    测试注册与远程调用

---
+++ src/test/java/org/example/TestDemoApplicationTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class TestDemoApplicationTest {
+
+    @Test
+    public void demo() {
+        System.out.println("true = " + true);
+    }
+
+}
