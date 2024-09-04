dcd52c260faa659f82e3ad93ddcedaf6803f26e4
chenlu <1481094533@qq.com>
2024-09-04 09:29:28 +0800

    item name

--- src/main/java/org/example/CommonsApplication.java
+++ src/main/java/org/example/CommonsApplication.java
@@ -4,18 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@EnableEurekaClient
 @EnableSwagger2
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = {"org.example.feign.**"})
+//@EnableFeignClients(basePackages = {"org.example.feign.**"})
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class TestDemoApplication {
+public class CommonsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestDemoApplication.class, args);
+        SpringApplication.run(CommonsApplication.class, args);
     }
 
 }
